502910a55ec59e4b2171e4cbdf489e7f246b14ca
VetoForest <vetosew@gmail.com>
2021-11-29 11:04:12 +0200

    qwe

--- sas.cs
+++ sas.cs
@@ -4,7 +4,7 @@ public class Class1
 {
 	public Class1()
 	{
-		Console.WriteLine("wdwqe");
+		Console.WriteLine("wdwqe");//.../////
 		Console.WriteLine("wdwqe");///////////
 		Console.WriteLine("wdwqe");/////////
 
